6731f7eb6d0747d711bae7e5d319d141784e39a2
bhoidhara <bhoidhara2008@gmail.com>
2026-01-26 14:22:22 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -6,15 +6,23 @@ c = conn.cursor()
 c.execute("""
 CREATE TABLE IF NOT EXISTS attendance(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-phone TEXT,
+enroll TEXT,
+name TEXT,
+class TEXT,
 time TEXT,
-lat TEXT,
-lon TEXT,
-mode TEXT
+status TEXT
 )
 """)
 
+c.execute("""
+CREATE TABLE IF NOT EXISTS teacher(
+email TEXT,
+password TEXT
+)
+""")
+
+c.execute("INSERT OR IGNORE INTO teacher VALUES ('teacher@parul.com','1234')")
+
 conn.commit()
 conn.close()
-
 print("Database ready")
